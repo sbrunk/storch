5857d91d1a96ce1cd13791c6b3a3fb5bc73efa37
sbrunk <sbrunk@users.noreply.github.com>
2024-02-05 20:02:23 +0000

    deploy: c7c769cd375268330f92b08a7e498e6b8c36b157

---
+++ js/render-katex.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    renderMathInElement(document.body, {
+      // customised options
+      // • auto-render specific keys, e.g.:
+      delimiters: [
+          {left: '$$', right: '$$', display: true},
+          {left: '$', right: '$', display: false},
+      ],
+      // • rendering keys, e.g.:
+      throwOnError : false
+    });
+});
